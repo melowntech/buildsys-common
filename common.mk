7c3b9c96a70e7884a735edfec3d883cbf6e04643
Václav Blažek <vaclav.blazek@citationtech.net>
2013-10-17 10:30:08 +0000

    shared/tools/buildsys: fixed common stuff
    git-svn-id: https://scm.citationtech.net/trunk/shared/tools/buildsys/common@6415 b7e6147c-0f76-4c84-a09d-19069afaf10c

--- common.mk
+++ common.mk
@@ -4,22 +4,24 @@ BUILDYS_COMMON_INCLUDED=1
 BUILDSYS_COMMON_ROOT := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
 
 # dput configuration
-DPUT_DISTRIBUTION = citationtech
-DPUT_CONFIG = $(BUILDSYS_COMMON_ROOT)/dput.cf
+DPUT_DISTRIBUTION := citationtech
+DPUT_CONFIG := dput.cf
 
 deb:
 	@(dpkg-buildpackage -b -j`grep -c ^processor /proc/cpuinfo`)
 
+debclean:
+	@(unset MAKELEVEL; unset MAKEFLAGS;	fakeroot ./debian/rules clean)
+
 dput:
-	dput -c $(DPUT_CONFIG) $(DPUT_DISTRIBUTION) $(call deb_changes_file)
+	dput -c $(join $(BUILDSYS_COMMON_ROOT),dput.cf) $(DPUT_DISTRIBUTION) $(call deb_changes_file)
 
 # notice no quotes around deb_tag -> we get package name and package version as
 # separates args
 dtag:
 	@$(BUILDSYS_COMMON_ROOT)/dtag.sh $(call deb_tag)
 
-debclean:
-	@(unset MAKELEVEL; unset MAKEFLAGS;	fakeroot ./debian/rules clean)
+.PHONY: deb debclean dput dtag
 
 # supporting macros
 define deb_changes_file
