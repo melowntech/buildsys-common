788039503a33e1ef5e5f3cb0f3c679228f67ec61
Václav Blažek <vaclav.blazek@citationtech.net>
2013-11-01 15:52:13 +0000

    shared/tools/buildsystem/common: added dversion target
    git-svn-id: https://scm.citationtech.net/trunk/shared/tools/buildsys/common@6564 b7e6147c-0f76-4c84-a09d-19069afaf10c

--- common.mk
+++ common.mk
@@ -16,6 +16,9 @@ debclean:
 dput:
 	dput -c $(join $(BUILDSYS_COMMON_ROOT),dput.cf) $(DPUT_DISTRIBUTION) $(call deb_changes_file)
 
+dversion:
+	@echo $(call deb_version)
+
 # notice no quotes around deb_tag -> we get package name and package version as
 # separates args
 dtag:
@@ -33,4 +36,9 @@ endef
 define deb_tag
 $(shell dpkg-parsechangelog | \
 	gawk '/^Version:/ { version=$$2; } /^Source:/ { source=$$2; } END { printf("%s %s\n", source, version); }')
+endef
+
+define deb_version
+$(shell dpkg-parsechangelog | \
+	gawk '/^Version:/ { print $$2; }')
 endef
