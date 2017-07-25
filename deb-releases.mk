f8c8c95e149b6970a4e32c1a19a73a3e0d76436d
Vaclav Blazek <vaclav.blazek@citationtech.net>
2017-07-25 14:19:13 +0200

    added support for templatized debian build

---
+++ deb-releases.mk
@@ -0,0 +1,7 @@
+# translate table for distributions
+
+DEB_TRANSLATE_RELEASE_wheezy = citationtech-wheezy
+DEB_TRANSLATE_RELEASE_jessie = melown-jessie
+DEB_TRANSLATE_RELEASE_xenial = melown-xenial
+DEB_TRANSLATE_RELEASE_yakkety = melown-yakkety
+DEB_TRANSLATE_RELEASE_zesty = melown-zesty
