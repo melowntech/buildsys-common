090ee64f73bac6f1dae52dc235b60502dbe5cd50
Vaclav Blazek <vaclav.blazek@citationtech.net>
2018-01-18 17:10:59 +0100

    added more debian/ubuntu releases

--- deb-releases.mk
+++ deb-releases.mk
@@ -2,6 +2,10 @@
 
 DEB_TRANSLATE_RELEASE_wheezy = citationtech-wheezy
 DEB_TRANSLATE_RELEASE_jessie = melown-jessie
+DEB_TRANSLATE_RELEASE_stretch = melown-stretch
+DEB_TRANSLATE_RELEASE_buster = melown-buster
 DEB_TRANSLATE_RELEASE_xenial = melown-xenial
 DEB_TRANSLATE_RELEASE_yakkety = melown-yakkety
 DEB_TRANSLATE_RELEASE_zesty = melown-zesty
+DEB_TRANSLATE_RELEASE_artful = melown-artful
+DEB_TRANSLATE_RELEASE_bionic = melown-bionic
