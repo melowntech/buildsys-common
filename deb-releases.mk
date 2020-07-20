22fccb0e700af5a54b02c45d6bf49fae8d90ea57
Vaclav Blazek <vaclav.blazek@citationtech.net>
2020-07-20 12:55:02 +0200

    deb packaging: added bullseye and focal, removed non-LTS debian version (yakkety, zesty and artful)

--- deb-releases.mk
+++ deb-releases.mk
@@ -1,11 +1,13 @@
 # translate table for distributions
 
+# Debian
 DEB_TRANSLATE_RELEASE_wheezy = citationtech-wheezy
 DEB_TRANSLATE_RELEASE_jessie = melown-jessie
 DEB_TRANSLATE_RELEASE_stretch = melown-stretch
 DEB_TRANSLATE_RELEASE_buster = melown-buster
+DEB_TRANSLATE_RELEASE_buster = melown-bullseye
+
+# Ubuntu
 DEB_TRANSLATE_RELEASE_xenial = melown-xenial
-DEB_TRANSLATE_RELEASE_yakkety = melown-yakkety
-DEB_TRANSLATE_RELEASE_zesty = melown-zesty
-DEB_TRANSLATE_RELEASE_artful = melown-artful
 DEB_TRANSLATE_RELEASE_bionic = melown-bionic
+DEB_TRANSLATE_RELEASE_focal = melown-focal
