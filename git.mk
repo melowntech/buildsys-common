9b4a63fa5f3b9fe29f34a910a00ee9f7264a99f5
Vaclav Blazek <vaclav.blazek@citationtech.net>
2022-03-11 13:43:06 +0100

    added support for editing git-branch config

--- git.mk
+++ git.mk
@@ -6,6 +6,17 @@ define git-hash
 $(shell $(BUILDSYS_COMMON_ROOT)/git/hash.sh)
 endef
 
+GIT_BRANCH_CONFIG_DIR = common.mk.d/git-branch/$(git-branch)
+GIT_BRANCH_CONFIG = ${GIT_BRANCH_CONFIG_DIR}/common.mk
+
 # include git-based config
-common.mk.d/git-branch/$(git-branch)/common.mk:
--include common.mk.d/git-branch/$(git-branch)/common.mk
+$(GIT_BRANCH_CONFIG):
+-include $(GIT_BRANCH_CONFIG)
+
+# edit git-branch config file
+$(GIT_BRANCH_CONFIG_DIR):
+	mkdir -p $(GIT_BRANCH_CONFIG_DIR)
+
+edit-git-branch-config: $(GIT_BRANCH_CONFIG_DIR)
+	sensible-editor $(GIT_BRANCH_CONFIG)
+.PHONY: edit-git-branch-config
